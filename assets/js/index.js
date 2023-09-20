0a98ca9161ceff966b79724317f0b84e03ab395f
mkhkshibata <mkhk.shibata@gmail.com>
2023-09-20 15:22:05 +0900

    一旦完成

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,30 @@
+//メニューボタン
 const menuBtn = document.querySelector('.menuBtn > a');
-const menuBtnTarget =document.querySelector('.menuWrap');
 
-menuBtn.addEventListener("click", function() {
-	menuBtnTarget.classList.toggle('active');
-} );
+if (menuBtn !== null) {
+	const menuBtnTarget =document.querySelector('.menuWrap');
+
+	menuBtn.addEventListener("click", function() {
+		menuBtnTarget.classList.toggle('active');
+	} );
+}
+
+
+//スムーススクロール
+const smoothScrollTrigger = document.querySelectorAll('a[href^="#"]');
+for (let i = 0; i < smoothScrollTrigger.length; i++){
+	smoothScrollTrigger[i].addEventListener('click', (e) => {
+		e.preventDefault();
+		let href = smoothScrollTrigger[i].getAttribute('href');
+		let targetElement = document.getElementById(href.replace('#', ''));
+		if (targetElement === null ) return;
+		const rect = targetElement.getBoundingClientRect().top;
+		const offset = window.pageYOffset;
+		const gap = 60;
+		const target = rect + offset - gap;
+		window.scrollTo({
+			top: target,
+			behavior: 'smooth',
+		});
+	});
+}
